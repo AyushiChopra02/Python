0eef00e42667ac9d6c575a838e72554708634c5c
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-09-02 20:43:11 +0530

    add

---
+++ CodingNinja/boolean.py
@@ -0,0 +1,16 @@
+#boolean
+c = True
+print(type(c))
+
+# relation operator
+a=10
+b=20
+print(a > b)
+
+#if else
+n= int(input())
+if n %2 == 0:
+    print("even")
+else:
+    print("odd")   
+
---
+++ CodingNinja/largestNumber.py
@@ -0,0 +1,31 @@
+a= int(input())
+b= int(input())
+c= int(input())
+if a>b and a>c:
+    print("a is largest")
+elif b>a and b>c:
+    print("b is largest")
+else:
+    print("c is largest")
+
+
+
+    # nested if else
+n= int(input())
+m = int(input())
+if n%2==0 :
+    if m%2==0 :
+        print(1)
+    else:
+        print(3) 
+else :
+    print(9)           
+
+
+
+#while
+n = int(input())
+count = 1
+while count <= n :
+    print(count)
+    count = count + 1 
