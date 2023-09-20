2b4103f86e8c176d890deb72d01c7f85c26aefdb
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-09-20 19:51:05 +0530

    add

---
+++ CodingNinja/fibbo.py
@@ -0,0 +1,17 @@
+def checkMember(n):
+    a = 0
+    b = 1
+    while a < n:
+        c = a + b
+        a = b
+        b = c
+    if a == n:
+        return True
+    else:
+        return False
+        
+n=int(input())
+if(checkMember(n)):
+    print("true")
+else:
+    print("false")
