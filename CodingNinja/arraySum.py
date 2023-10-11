5cb6706ebdb71f4c1e2db50d9fd84825a2a80813
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-11 23:48:37 +0530

    add

---
+++ CodingNinja/arraySum.py
@@ -0,0 +1,8 @@
+# Read input as sepcified in the question
+# Print output as specified in the question
+n=int(input())
+li=[int(x) for x in input().split()]
+sum=0
+for i in range(n):
+    sum=sum+li[i]
+print(sum)
