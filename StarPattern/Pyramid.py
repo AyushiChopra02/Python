b2f277447a15d152fcc263d63be5df857b8df61d
Ayushi <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-16 23:51:07 +0530

    Update Pyramid.py

--- StarPattern/Pyramid.py
+++ StarPattern/Pyramid.py
@@ -2,6 +2,6 @@ for i in range (1,5) :
     for j in range(1,8):
         if j>=5-i and j<=3+i :
             print("*" , end='')
-        else:
+        else :
             print(" " , end ='')
     print()
